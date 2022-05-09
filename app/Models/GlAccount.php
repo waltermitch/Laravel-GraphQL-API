6f4e5c0b049c3538c054314ea1338f677a81fcf5
waltermitch <devmitch256@gmail.com>
2022-05-09 21:10:06 +0300

    add gll accounts/gl type codes

---
+++ app/Models/GlAccount.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class GlAccount extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+
+    ];
+
+    /**
+     * Get parent account for the child account.
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    /**
+     * Get child account for the parent account.
+     */
+    public function child()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    /**
+     * Get the gl type code that owns the gl account.
+     */
+    public function glTypeCode(): BelongsTo
+    {
+        return $this->belongsTo(GlTypeCode::class);
+    }
+}
