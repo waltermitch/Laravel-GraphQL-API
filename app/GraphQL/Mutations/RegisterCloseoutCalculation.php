b9fb5ce184106530a533a8609c3cb385b7397dc3
waltermitch <devmitch256@gmail.com>
2022-06-07 20:55:01 +0200

    add register clouseout calculations

--- app/GraphQL/Mutations/RegisterCloseoutCalculation.php
+++ app/GraphQL/Mutations/RegisterCloseoutCalculation.php
@@ -16,43 +16,56 @@ class RegisterCloseoutCalculation
 
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        //TO-DO PECZIS
-        
-        //register closeout calculations
-        
-        // $unit_id = $args['unit_id'];
-        // $period_id = $args['period_id'];
-        // $fields = $args['fields'];
-        
-        // $fields array has exactly those keys:
-        // 'non_resetable', 'net_o_v', 'net_charge', 'tax_from_the_tape', 'net_voucher', 
-        // 'overring_void_tax', 'charge_tax', 'voucher_tax', 'total_petty_cash', 'actual_cash_deposit', 'customer_count_breakfast',
-        // 'net_sales_breakfast', 'customer_count_lunch', 'net_sales_lunch', 'customer_count_dinner', 'net_sales_dinner'
-        
-        // after calculations returned array has to have exact structure:
+        $lastClosedRegister = \App\Models\RegisterCloseout::where('period_id', $args['period_id'])
+            ->where('unit_id', $args['unit_id'])
+            ->where('register_id', $args['register_id'])
+            ->orderBy('created_at', 'DESC')
+            ->first();
+
+        $fields = $args['fields'];
+
+        $totalToDistribute = $fields['non_resetable'];
+        $netTotal = $totalToDistribute - $fields['tax_from_the_tape'];
+        $netCash = $netTotal - $fields['net_o_v'] - $fields['net_charge'] - $fields['net_voucher'];
+        $cashTax = $fields['tax_from_the_tape'] - $fields['overring_void_tax'] - $fields['charge_tax'] - $fields['voucher_tax'];
+        $customerCountTotal = $fields['customer_count_breakfast'] + $fields['customer_count_lunch'] + $fields['customer_count_dinner'];
+        $netSalesTotal = $fields['net_sales_breakfast'] + $fields['net_sales_lunch'] + $fields['net_sales_dinner'];
+
+        $totalDailyDeposit = \App\Models\RegisterCloseout::where('period_id', $args['period_id'])
+            ->where('unit_id', $args['unit_id'])
+            ->sum('actual_cash_deposit');
+
+        $totalNetSales = $totalToDistribute - $fields['tax_from_the_tape'] - $fields['net_o_v'] - $fields['net_voucher'];
+        $charge = $fields['net_charge'];
+        $cashTotal = $totalNetSales - $charge;
+        $pettyCash = $fields['total_petty_cash'];
+        $calculatedCashDeposit = $cashTotal + $cashTax - $pettyCash;
+        $actualDeposit = $fields['actual_cash_deposit'];
+        $overShort = $actualDeposit - $calculatedCashDeposit;
+
         return [
-            'fields' => [
-                'net_total' => 1,
-                'last_non_resettable' => 1,
-                'total_to_distribute' => 1,
-                'net_cash' => 1,
-                'cash_tax' => 1,
-                'calculated_cash_deposit' => 1,
-                'over_short' => 1,
-                'customer_count_totals' => 1,
-                'net_sales_total' => 1
+            'fields'     => [
+                'net_total'               => $netTotal,
+                'last_non_resettable'     => $lastClosedRegister ? $lastClosedRegister->non_resetable : 0,
+                'total_to_distribute'     => $totalToDistribute,
+                'net_cash'                => $netCash,
+                'cash_tax'                => $cashTax,
+                'calculated_cash_deposit' => $calculatedCashDeposit,
+                'over_short'              => $overShort,
+                'customer_count_totals'   => $customerCountTotal,
+                'net_sales_total'         => $netSalesTotal,
             ],
             'statistics' => [
-                'total_daily_deposit' => 1,
-                'total_net_sales' => 1,
-                'charge' => 1,
-                'cash_total' => 1,
-                'cash_tax' => 1,
-                'petty_cash' => 1,
-                'calced_deposit' => 1,
-                'actual_deposit' => 1,
-                'over_short' => 1
-            ]
+                'total_daily_deposit' => $totalDailyDeposit,
+                'total_net_sales'     => $totalNetSales,
+                'charge'              => $charge,
+                'cash_total'          => $cashTotal,
+                'cash_tax'            => $cashTax,
+                'petty_cash'          => $pettyCash,
+                'calced_deposit'      => $calculatedCashDeposit,
+                'actual_deposit'      => $actualDeposit,
+                'over_short'          => $overShort,
+            ],
         ];
     }
 }
