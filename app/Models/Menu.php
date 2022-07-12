413683dd9b2178a96e2e52888ccff409b39ee94a
waltermitch <devmitch256@gmail.com>
2022-07-12 14:56:33 -0400

    Added migration files for new role and menu relations

---
+++ app/Models/Menu.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+
+    /**
+     * Get the users for the role.
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
